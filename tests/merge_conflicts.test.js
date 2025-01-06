b4a8db08f0f59330d85f9797b70981a7dbb6d840
k-9 <siripifrancis21@gmail.com>
2025-01-06 23:53:08 +0000

    Update merge_conflicts.test.js

--- tests/merge_conflicts.test.js
+++ tests/merge_conflicts.test.js
@@ -1,5 +1,5 @@
-const feature1 = require('./feature1');
-const feature2 = require('./feature2');
+import feature1 from '../src/components/feature1';
+import feature2 from '../src/components/feature2';
 
 feature1(); // Should log: Feature 1 is implemented successfully!
 feature2(); // Should log: Feature 2 is implemented successfully!
